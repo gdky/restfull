91a1278ac8cea29ad87bfe5d2a52effde9da0c84
bubleSun <1014077044@qq.com>
2016-09-09 17:26:52 +0800

    #090901

--- src/main/java/gov/gdgs/zs/dao/AddzyswsnjDao.java
+++ src/main/java/gov/gdgs/zs/dao/AddzyswsnjDao.java
@@ -57,7 +57,7 @@ public class AddzyswsnjDao extends BaseDao {
 			return obj;
 		}
     
-    public Map<String, Object> getzyswsnjbById(String id) {
+   /* public Map<String, Object> getzyswsnjbById(String id) {
 		String sql = "select a.id,a.ND,c.XMING,"
 				+ "b.dwmc, CASE a.ZTDM WHEN 0 THEN '退回' "
 				+ "WHEN 1 THEN '保存' "
@@ -65,9 +65,31 @@ public class AddzyswsnjDao extends BaseDao {
 				+ " WHEN 3 THEN '年检' "
 				+ " ELSE NULL END AS ZTDM "
 				+ "  from zs_zcswsnj a,zs_jg b,zs_ryjbxx c, zs_zysws  d "
-				+ "  where a.SWS_ID=d.ID and c.ID=d.RY_ID and a.id=? ";
+				+ "  where a.SWS_ID=d.ID and c.ID=d.RY_ID and a.ZSJG_ID=b.ID and a.id=? ";
 		Map<String,Object> rs = jdbcTemplate.queryForMap(sql, id);
 		return rs;
+	}*/
+    
+    public Map<String,Object> getzyswsnjbById(String id){
+		StringBuffer sb = new StringBuffer();
+		sb.append("	select max( a.nd),a.nd,a.ID,c.XMING,c.DHHM,d.ZYZSBH,d.ZYZGZSBH,");
+		sb.append("	b.dwmc,a.swsfzryj,e1.MC as xb,f1.mc as xl,a.CZBL,DATE_FORMAT(a.SWSFZRSJ,'%Y-%m-%d') AS SWSFZRSJ,a.SWSFZR,");
+		sb.append("	DATE_FORMAT(c.SRI,'%Y-%m-%d') AS SRI,c.SFZH,a.BAFS,a.ZJWGDM,a.NJWGDM,a.ZJ,a.SWSFZRYJ,");
+		sb.append("	CASE a.ZTDM WHEN 1 THEN '保存'  WHEN 2 THEN '未审批' WHEN 0 THEN '退回' WHEN 3 THEN '已年检' ELSE NULL END AS njzt,");
+		sb.append("	DATE_FORMAT( f.SPSJ,'%Y-%m-%d') AS spsj,f.SPYJ,f.SPRNAME");
+		sb.append("	FROM  ( zs_zcswsnj a left join(zs_jg b,zs_ryjbxx c,zs_zysws d,dm_xb e1,dm_xl f1) on (");
+		sb.append("	a.ZSJG_ID=b.ID AND c.id=d.RY_ID");
+		sb.append("	and c.XL_DM=f1.id and c.XB_DM=e1.ID");
+		sb.append("	AND d.id = a.sws_id ) ) left join(zs_spzx e,zs_spxx f,zs_splcbz g,zs_splc h) on (");
+		sb.append("	f.SPID=e.ID AND g.ID=f.LCBZID and a.ZTDM <> 1");
+		sb.append("	AND h.ID=g.LCID AND h.LCLXID='12'");
+		sb.append("	AND a.ID=e.SJID )");
+		sb.append("	WHERE  a.id=? group by nd");
+		 Map<String,Object> rs = this.jdbcTemplate.queryForMap(sb.toString(),new Object[]{id});
+		// Map<String, Object> ob = new HashMap<>();
+			//ob.put("data", rs);
+		 
+		 return rs;
 	}
 	
 }
