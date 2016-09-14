b72a7f62628778e306dcc6e4f8025ab69f05624d
Fsenxu <fasen.xu@gdkyit.com>
2016-09-14 16:08:20 +0800

    #914

--- src/main/java/gov/gdgs/zs/api/ZzsdController.java
+++ src/main/java/gov/gdgs/zs/api/ZzsdController.java
@@ -58,9 +58,10 @@ public class ZzsdController {
 			@RequestBody Map<String,Object> rqbody,
 			HttpServletRequest request){ 
 		User user =  accountService.getUserFromHeaderToken(request);
+		Integer lx = (Integer)rqbody.get("lx");
 		String sdyy = (String)rqbody.get("sdyy");
-		List<String> jgId = (List<String>) rqbody.get("jgId");//jgId:['wrwer','34423','dsa3d']
-		zzsdService.addJgZzsd(user,sdyy,jgId);
+		List<String> jgId = (List<String>) rqbody.get("jgId");
+		zzsdService.addJgZzsd(user,sdyy,jgId,lx);
 		ResponseMessage rm  = new ResponseMessage(ResponseMessage.Type.success, "200", "更新成功");
 		return new ResponseEntity<>(rm,HttpStatus.OK);
 	}
