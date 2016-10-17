c8a2c56ad5a839023dc0817b28a8907bcfb1d743
neko <mervyn.chung@foxmail.com>
2016-10-17 15:53:58 +0800

    1017#01

--- src/main/java/gov/gdgs/zs/dao/AddsdsbDao.java
+++ src/main/java/gov/gdgs/zs/dao/AddsdsbDao.java
@@ -70,7 +70,7 @@ public class AddsdsbDao extends BaseJdbcDao  implements IAddsdsbDao{
 		sb.append(" SELECT  SQL_CALC_FOUND_ROWS @rownum:=@rownum+1 AS 'key',t.*");
 		sb.append(" FROM   ( select a.id,b.DWMC,a.nd,a.FRDBXM,a.CZRS,a.HHRS,a.ZYZCSWSRS,a.RYZS,a.ZCZJ,a.YYSR,");	
 		sb.append(" case a.JGXZ_DM when 1 then '合伙事务所' when 2 then '有限公司' when 3 then '无'  else null end as JGXZ,");	
-		sb.append(" case a.ZTBJ when 0 then '保存' when 2 then '提交' else '未知' end as ZTBJ");		
+		sb.append(" case a.ZTBJ when 1 then '提交' when 2 then '通过' when 0 then '保存' when 3 then '退回' else null end as ZTBJ");		
 		sb.append(" FROM " + "zs_sdsb_swsjbqk a,zs_jg b,(SELECT @rownum:=?) temp");
 		sb.append(condition.getSql());// 相当元 where b.DWMC like '%%'
 		sb.append(" AND a.JG_ID=b.ID  and a.JG_ID=? ORDER BY a.nd DESC ) AS t");
--- src/main/java/gov/gdgs/zs/service/AddsdsbService.java
+++ src/main/java/gov/gdgs/zs/service/AddsdsbService.java
@@ -22,154 +22,167 @@ import com.gdky.restfull.exception.BbtbException;
 import com.gdky.restfull.utils.Common;
 
 @Service
-public class AddsdsbService implements IAddsdsbService{
-	
+public class AddsdsbService implements IAddsdsbService {
+
 	@Resource
 	private ClientsdsbDao clientSdsbDao;
 	@Resource
 	private IAddsdsbDao iaddsdsbDao;
-	
+
 	@Resource
 	private AddsdsbDao addsdsbDao;
-	
+
 	@Override
-	public Map<String, Object> AddSwsjbqkb (Map<String, Object> obj) {	
-		Map<String,Object> map = new LinkedHashMap<String,Object>();
-		//判断是否已存在同年的基本情况表
-		if(clientSdsbDao.isExists(obj.get("nd"),obj.get("jg_id"),"zs_sdsb_swsjbqk")){
+	public Map<String, Object> AddSwsjbqkb(Map<String, Object> obj) {
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		// 判断是否已存在同年的基本情况表
+		if (clientSdsbDao.isExists(obj.get("nd"), obj.get("jg_id"),
+				"zs_sdsb_swsjbqk")) {
 			throw new BbtbException("该年度报表已存在，请勿重复添加");
 		}
-		String rs= addsdsbDao.AddSwsjbqkb(obj);
+		String rs = addsdsbDao.AddSwsjbqkb(obj);
 		map.put("id", rs);
 		return map;
 
 	}
-	
+
 	@Override
 	public void UpdateSwsjbqkb(Map<String, Object> obj) {
 		iaddsdsbDao.UpdateSwsjbqkb(obj);
 	}
-	
-	 
-		public Map<String, Object> getSwsjbqkb(int page, int pageSize,int Jgid,int Id, String whereParam) {
-			Map<String, Object> where = Common.decodeURItoMap(whereParam);		
-			Map<String, Object> rs = addsdsbDao.getSwsjbqkb(page, pageSize, Jgid,Id, where);
-			return rs;
-		}
-		public Map<String, Object> getSwsjbqkbById(String id) {
-			Map<String,Object> obj = addsdsbDao.getSwsjbqkbById(id);
-			return obj;
-		}
-		public Map<String, Object> getSwsjbqkInit(User user) {
-			
-			//获取去年年度
-			Calendar cal = Calendar.getInstance();
-			int last_y = cal.get(Calendar.YEAR) - 1;
-			int now_y = cal.get(Calendar.YEAR);
-			//获取事务所名称
-			//获取事务所执业税务师人数
-			//获取事务所性质
-			List<Map<String,Object>> ls = clientSdsbDao.getSwsTj(user.getJgId(),last_y);
-			if(ls.size() == 0 ){
-				ls = clientSdsbDao.getSwsTj(user.getJgId(),now_y);
-				if (ls.size() == 0 ){
-					throw new BbtbException("无法获得基本数据");
-				}
-			}
-			Map<String,Object> swstj = ls.get(0);
-			//获取去年全年营业收入
-			BigDecimal srze = clientSdsbDao.getSrze(last_y,user.getJgId());
-			if(srze == null) {
-				throw new BbtbException("没法获取"+last_y+"年主营收入总额，请先提交该年度利润表");
-			}
-			Map<String,Object> obj = new HashMap<String,Object>();
-			obj.put("nd", last_y);
-			obj.put("dwmc", swstj.get("dwmc"));
-			obj.put("jgxz_dm",String.valueOf(swstj.get("jgxz_dm")));
-			obj.put("zyzcswsrs", swstj.get("zysws_sfnum"));
-			obj.put("srze", srze);
-			return obj;
-		}
-		
-		@Override
-		public Map<String, Object> AddJygmtjb (Map<String, Object> obj) {	
-			Map<String,Object> map = new LinkedHashMap<String,Object>();
-			String rs= iaddsdsbDao.AddJygmtjb(obj);
-			map.put("id", rs);
-			return map;
 
-		}
-		
-		@Override
-		public void UpdateJygmtjb(Map<String, Object> obj) {
-			iaddsdsbDao.UpdateJygmtjb(obj);
-		}
-		
-		
-		//经营规模统计表
-		public Map<String, Object> getJygmtjb(int page, int pageSize,int Id,int Jgid, String where) {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			if (where != null) {
-				try {
-					where = java.net.URLDecoder.decode(where, "UTF-8");
-					ObjectMapper mapper = new ObjectMapper();
-					map = mapper.readValue(where,
-							new TypeReference<Map<String, Object>>() {
-							});
-				} catch (Exception e) {
-				}
-			}		
-			Map<String, Object> rs = addsdsbDao.getJygmtjb(page, pageSize,Id,Jgid, map);
-			return rs;
-		}
-		public Map<String, Object> getJygmtjbById(String id) {
-			Map<String,Object> obj = addsdsbDao.getJygmtjbById(id);
-			return obj;
-		}
-		
-		public Map<String, Object> getOK1(String jgid) {
-			Map<String,Object> obj = addsdsbDao.getOk(jgid);
-			return obj;
-		}
-		
-		//鉴证业务情况统计表
-		
-		@Override
-		public Map<String, Object> AddJzywqktjb (Map<String, Object> obj) {	
-			Map<String,Object> map = new LinkedHashMap<String,Object>();
-			String rs= iaddsdsbDao.AddJzywqktjb(obj);
-			map.put("id", rs);
-			return map;
+	public Map<String, Object> getSwsjbqkb(int page, int pageSize, int Jgid,
+			int Id, String whereParam) {
+		Map<String, Object> where = Common.decodeURItoMap(whereParam);
+		Map<String, Object> rs = addsdsbDao.getSwsjbqkb(page, pageSize, Jgid,
+				Id, where);
+		return rs;
+	}
 
+	public Map<String, Object> getSwsjbqkbById(String id) {
+		Map<String, Object> obj = addsdsbDao.getSwsjbqkbById(id);
+		return obj;
+	}
+
+	public Map<String, Object> getSwsjbqkInit(User user) {
+
+		// 获取去年年度
+		Calendar cal = Calendar.getInstance();
+		int last_y = cal.get(Calendar.YEAR) - 1;
+		int now_y = cal.get(Calendar.YEAR);
+		// 获取事务所名称
+		// 获取事务所执业税务师人数
+		// 获取事务所性质
+		List<Map<String, Object>> ls = clientSdsbDao.getSwsTj(user.getJgId(),
+				last_y);
+		if (ls.size() == 0) {
+			ls = clientSdsbDao.getSwsTj(user.getJgId(), now_y);
+			if (ls.size() == 0) {
+				throw new BbtbException("无法获得基本数据");
+			}
 		}
-		
-		@Override
-		public void UpdateJzywqktjb(Map<String, Object> obj) {
-			iaddsdsbDao.UpdateJzywqktjb(obj);
+		Map<String, Object> swstj = ls.get(0);
+		// 获取去年全年营业收入
+		BigDecimal srze = clientSdsbDao.getSrze(last_y, user.getJgId());
+		if (srze == null) {
+			throw new BbtbException("没法获取" + last_y + "年主营收入总额，请先提交该年度利润表");
 		}
-		public Map<String, Object> getJzywqktjb(int page, int pageSize,int Id,int Jgid, String where) {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			if (where != null) {
-				try {
-					where = java.net.URLDecoder.decode(where, "UTF-8");
-					ObjectMapper mapper = new ObjectMapper();
-					map = mapper.readValue(where,
-							new TypeReference<Map<String, Object>>() {
-							});
-				} catch (Exception e) {
-				}
-			}		
-			Map<String, Object> rs = addsdsbDao.getJzywqktjb(page, pageSize, Id,Jgid,map);
-			return rs;
+		BigDecimal b1 = new BigDecimal(10000);
+		srze = srze.divide(b1, 2, BigDecimal.ROUND_HALF_UP);
+		Map<String, Object> obj = new HashMap<String, Object>();
+		obj.put("nd", last_y);
+		obj.put("dwmc", swstj.get("dwmc"));
+		obj.put("jgxz_dm", String.valueOf(swstj.get("jgxz_dm")));
+		obj.put("zyzcswsrs", swstj.get("zysws_sfnum"));
+		obj.put("srze", srze);
+		return obj;
+	}
+
+	@Override
+	public Map<String, Object> AddJygmtjb(Map<String, Object> obj) {
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		String rs = iaddsdsbDao.AddJygmtjb(obj);
+		map.put("id", rs);
+		return map;
+
+	}
+
+	@Override
+	public void UpdateJygmtjb(Map<String, Object> obj) {
+		iaddsdsbDao.UpdateJygmtjb(obj);
+	}
+
+	// 经营规模统计表
+	public Map<String, Object> getJygmtjb(int page, int pageSize, int Id,
+			int Jgid, String where) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if (where != null) {
+			try {
+				where = java.net.URLDecoder.decode(where, "UTF-8");
+				ObjectMapper mapper = new ObjectMapper();
+				map = mapper.readValue(where,
+						new TypeReference<Map<String, Object>>() {
+						});
+			} catch (Exception e) {
+			}
 		}
-		public Map<String, Object> getJzywqktjbById(String id) {
-			Map<String,Object> obj = addsdsbDao.getJzywqktjbById(id);
-			return obj;
+		Map<String, Object> rs = addsdsbDao.getJygmtjb(page, pageSize, Id,
+				Jgid, map);
+		return rs;
+	}
+
+	public Map<String, Object> getJygmtjbById(String id) {
+		Map<String, Object> obj = addsdsbDao.getJygmtjbById(id);
+		return obj;
+	}
+
+	public Map<String, Object> getOK1(String jgid) {
+		Map<String, Object> obj = addsdsbDao.getOk(jgid);
+		return obj;
+	}
+
+	// 鉴证业务情况统计表
+
+	@Override
+	public Map<String, Object> AddJzywqktjb(Map<String, Object> obj) {
+		Map<String, Object> map = new LinkedHashMap<String, Object>();
+		String rs = iaddsdsbDao.AddJzywqktjb(obj);
+		map.put("id", rs);
+		return map;
+
+	}
+
+	@Override
+	public void UpdateJzywqktjb(Map<String, Object> obj) {
+		iaddsdsbDao.UpdateJzywqktjb(obj);
+	}
+
+	public Map<String, Object> getJzywqktjb(int page, int pageSize, int Id,
+			int Jgid, String where) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if (where != null) {
+			try {
+				where = java.net.URLDecoder.decode(where, "UTF-8");
+				ObjectMapper mapper = new ObjectMapper();
+				map = mapper.readValue(where,
+						new TypeReference<Map<String, Object>>() {
+						});
+			} catch (Exception e) {
+			}
 		}
-		
-		public Map<String, Object> getUpyear(String jgid){
-			Map<String, Object >obj=addsdsbDao.getUpyear(jgid);
-			return obj;
-		}
+		Map<String, Object> rs = addsdsbDao.getJzywqktjb(page, pageSize, Id,
+				Jgid, map);
+		return rs;
+	}
+
+	public Map<String, Object> getJzywqktjbById(String id) {
+		Map<String, Object> obj = addsdsbDao.getJzywqktjbById(id);
+		return obj;
+	}
+
+	public Map<String, Object> getUpyear(String jgid) {
+		Map<String, Object> obj = addsdsbDao.getUpyear(jgid);
+		return obj;
+	}
 
 }
